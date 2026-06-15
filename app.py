2e58a54f5042afc409b2c2659e69ff2c01e7240a
akshaykhade-dev <khadeakshay109@gmail.com>
2026-06-15 11:54:10 +0000

    edit text app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World!</h1>
-            <p>This is my 1st project</p>
-            <h2>All done</h2>'
+    return '<h1>Hello, World!<h1>'
 
 
 app.run(host="0.0.0.0", port=5000)
